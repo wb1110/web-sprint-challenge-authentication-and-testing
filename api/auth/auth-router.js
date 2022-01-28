78c5737a98d0ded48e2239311e5013b9da31dc92
wb1110 <strengthcoachwb@gmail.com>
2022-01-28 12:16:18 -0600

    two end points tested

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -15,38 +15,21 @@ router.post(
         const hash = bcrypt.hashSync(password, 8)
         const newUser = { username, password: hash }
         const inserted = await Users.add(newUser)
-        res.json(inserted)
+        res.status(201).json(inserted)
       } catch (err) {
         next(err)
       }
 });
 
-router.post('/login', requiredFields, checkUsernameExists, (req, res, next) => {
-  /*
-    IMPLEMENT
-    You are welcome to build additional middlewares to help with the endpoint's functionality.
-
-    1- In order to log into an existing account the client must provide `username` and `password`:
-      {
-        "username": "Captain Marvel",
-        "password": "foobar"
-      }
-
-    2- On SUCCESSFUL login,
-      the response body should have `message` and `token`:
-      {
-        "message": "welcome, Captain Marvel",
-        "token": "eyJhbGciOiJIUzI ... ETC ... vUPjZYDSa46Nwz8"
-      }
-
-
-    4- On FAILED login due to `username` not existing in the db, or `password` being incorrect,
-      the response body should include a string exactly as follows: "invalid credentials".
-  */
+router.post(
+  '/login',
+  requiredFields,
+  checkUsernameExists,
+  (req, res, next) => {
       const { password } = req.body
       if (bcrypt.compareSync(password, req.user.password)) {
         const token = generateToken(req.user)
-        res.json({ message: "welcome, Captain Marvel", token })
+        res.status(200).json({ message: "welcome, Captain Marvel", token })
       } else {
         next({ status: 401, message: "invalid credentials" })
       }
--- api/server.test.js
+++ api/server.test.js
@@ -1,4 +1,77 @@
 // Write your tests here
+const request = require('supertest')
+const server = require('./server')
+const db = require('../data/dbConfig')
+
+beforeAll(async () => {
+  await db.migrate.rollback()
+  await db.migrate.latest()
+})
+afterAll(async () => {
+  await db.destroy()
+})
+
 test('sanity', () => {
   expect(true).toBe(false)
+})
+
+describe('POST /register', () => {
+  test('successful status and response', async () => {
+    const res = await request(server)
+    .post('/api/auth/register')
+    .send({ username: 'test', password: '1234' })
+    expect(res.status).toEqual(201)
+  })
+  test('username taken', async () => {
+    const res = await request(server)
+    .post('/api/auth/register')
+    .send({ username: 'test', password: '1234' })
+    expect(res.status).toEqual(422)
+  })
+  test('username taken', async () => {
+    const res = await request(server)
+    .post('/api/auth/register')
+    .send({ username: 'test', password: '1234' })
+    expect(res.status).toEqual(422)
+  })
+  test('missing information', async () => {
+    const res = await request(server)
+    .post('/api/auth/register')
+    .send({ username: 'test2' })
+    expect(res.status).toEqual(401)
+  })
+})
+
+describe('POST /login', () => {
+  beforeEach(async () => {
+    await request(server)
+    .post('/api/auth/register')
+    .send({ username: 'test', password: '1234' })
+  })
+  test('successful status and response', async () => {
+    const res = await request(server)
+    .post('/api/auth/login')
+    .send({ username: 'test', password: '1234' })
+    expect(res.status).toEqual(200)
+  })
+  test('username does not exist', async () => {
+    const res = await request(server)
+    .post('/api/auth/login')
+    .send({ username: 'test2', password: '1234' })
+    expect(res.status).toEqual(401)
+  })
+  test('missing information', async () => {
+    const res = await request(server)
+    .post('/api/auth/login')
+    .send({ username: 'test' })
+    expect(res.status).toEqual(401)
+  })
+  test('incorrect password', async () => {
+    const expected = {"message": "invalid credentials"};
+    const res = await request(server)
+    .post('/api/auth/login')
+    .send({ username: 'test', password: '12345' })
+    expect(res.status).toEqual(401)
+    expect(res.body).toEqual(expected)
+  })
 })
