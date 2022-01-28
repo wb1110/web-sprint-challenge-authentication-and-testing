41b61258e47c0f5596b449de927af625215e183a
wb1110 <strengthcoachwb@gmail.com>
2022-01-28 11:06:03 -0600

    restricted

--- api/middleware/restricted.js
+++ api/middleware/restricted.js
@@ -1,5 +1,19 @@
+const { JWT_SECRET } = require("../secrets");
+const jwt = require('jsonwebtoken')
+
 module.exports = (req, res, next) => {
-  next();
+  const token = req.headers.authorization
+  if (!token) {
+    return next({ status: 401, message: 'token required' })
+  }
+  jwt.verify(token, JWT_SECRET, (err, decodedToken) => {
+    if (err) {
+      next({ status: 401, message: 'token invalid' })
+    } else {
+      req.decodedToken = decodedToken
+      next()
+    }
+})
   /*
     IMPLEMENT
 
